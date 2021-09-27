fd5e4fdbebcbffd0f1c439382ef6cca877bbca0a
Some1AtEarth <90246985+Some1AtEarth@users.noreply.github.com>
2021-09-27 16:49:46 +1000

    Add files via upload

---
+++ Pro-c11-sa1-template-main/sketch.js
@@ -0,0 +1,27 @@
+var weight = [35,38,42,45,43,34,36,41,48,32];
+var average;
+var sum = 0
+
+function averages() {
+  for (var i = 0; i<weight.length; i++) {
+    sum = sum + weight[i]
+  }
+  average = sum/weight.length
+  console.log(average)
+
+
+}
+
+function setup() {
+  createCanvas(400,400);
+  averages()
+
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
